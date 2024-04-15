fd8e3185a028bddd6684fb3b02d15e3865657804
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-04-15 12:27:10 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,13 +19,5 @@ int main(int argc, char **argv) {
     // Default "splash".
     std::cout << "PACS - Second challenge - Andrea Di Antonio." << std::endl;
 
-    pacs::algebra::Matrix<double, pacs::algebra::Row> matrix{2, 2};
-
-    matrix(0, 0) = 4;
-    matrix(1, 1) = 3;
-
-    matrix.compress();
-    std::cout << matrix.norm<pacs::algebra::Frobenius>() << std::endl;
-
     return 0;
 }
