a7fc471a0c7f8a5a24909aa149fc1b66e31f503a
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-04-16 23:05:22 +0200

    Fancier test output and symmetric test

--- include/Timer.hpp
+++ include/Timer.hpp
@@ -23,23 +23,36 @@ namespace pacs {
          * @param vector 
          */
         template<typename T, Order O>
-        void timer(const Matrix<T, O> &matrix, const std::vector<T> &vector, const std::size_t &tests = 1E6) {
+        void timer(const Matrix<T, O> &matrix, const std::vector<T> &vector, const std::size_t &tests = 5E5) {
+
+            std::size_t products = 0;
 
             // Start.
+            std::cout << "\nTesting for Matrix x Vector product." << std::endl;
             auto start = std::chrono::high_resolution_clock::now();
 
-            for(std::size_t j = 0; j < tests; ++j)
-                matrix * vector;
+            if(matrix.columns() == vector.size()) {
+                std::cout << "- Testing Matrix x Vector." << std::endl;
+                ++products;
+
+                for(std::size_t j = 0; j < tests; ++j)
+                    matrix * vector;
+            }
+
+            if(matrix.rows() == vector.size()) {
+                std::cout << "- Testing Vector x Matrix." << std::endl;
+                ++products;
+                
+                for(std::size_t j = 0; j < tests; ++j)
+                    vector * matrix;
+            }
 
             // Stop.
             auto stop = std::chrono::high_resolution_clock::now();
 
             // Results.
-            std::cout << "\n" << tests << " products." << std::endl;
-            std::cout << matrix.rows() << " by " << matrix.columns() << " elements matrix times a " << vector.size() << " element(s) vector." << std::endl;
-            std::cout << "Sparsity: " << matrix.sparsity() << "." << std::endl;
-            std::cout << "Ordering: " << (O == Row ? "row-first" : "column-first") << "." << std::endl;
-            std::cout << "Compression: " << (matrix.is_compressed() ? "yes" : "no") << "." << std::endl;
+            std::cout << "\nTested " << products * tests << " (" << products << " x " << tests << ") products between a " << matrix.rows() << " by " << matrix.columns() << " sparse matrix and a " << vector.size() << " (by 1) vector." << std::endl;
+            std::cout << "Matrix has a sparsity of " << matrix.sparsity() << ", it is " << (O == Row ? "row-first" : "column-first") << " ordered and it is " << (matrix.is_compressed() ? "in" : "not in") << " compressed form." << std::endl;
             std::cout << "Elapsed time: " << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() / 1E6 << " second(s)." << std::endl;
 
         }
