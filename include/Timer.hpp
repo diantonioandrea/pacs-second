07a76684087d6008109018dc254f96dcc18e607a
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-04-19 11:38:46 +0200

    Added a Matrix x Matrix test

--- include/Timer.hpp
+++ include/Timer.hpp
@@ -25,7 +25,7 @@ namespace pacs {
     namespace algebra {
 
         /**
-         * @brief Prints the time needed for an arbitrary number of Matrix x Vector products.
+         * @brief Prints the time needed for an arbitrary number of Matrix x Vector products, along with some other info.
          * 
          * @tparam T 
          * @tparam O 
@@ -64,6 +64,38 @@ namespace pacs {
             // Results.
             std::cout << "\nTested " << products * tests << " (" << products << " x " << tests << ") products between a " << matrix.rows() << " by " << matrix.columns() << " sparse matrix and a " << vector.size() << " (by 1) vector." << std::endl;
             std::cout << "Matrix has a sparsity of " << matrix.sparsity() << ", it is " << (O == Row ? "row-first" : "column-first") << " ordered and it is " << (matrix.is_compressed() ? "in" : "not in") << " compressed form." << std::endl;
+            std::cout << "Elapsed time: " << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() / 1E6 << " second(s)." << std::endl;
+
+        }
+
+        /**
+         * @brief Prints the time needed for an arbitrary number of Matrix x Matrix products, along with some other info.
+         * 
+         * @tparam T 
+         * @tparam O 
+         * @param matrix 
+         * @param vector 
+         */
+        template<typename T, Order O>
+        void timer(const Matrix<T, O> &first, const Matrix<T, O> &second, const std::size_t &tests = 1E3) {
+
+            std::cout << "\nTesting for Matrix x Vector product." << std::endl;
+
+            // Tests.
+            auto start = std::chrono::high_resolution_clock::now();
+
+            std::cout << "- Testing Matrix x Matrix." << std::endl;
+
+            for(std::size_t j = 0; j < tests; ++j)
+                first * second;
+
+            auto stop = std::chrono::high_resolution_clock::now();
+
+            // Results.
+            std::cout << "\nTested " << tests << " products between a " << first.rows() << " by " << first.columns() << " sparse matrix and a " << second.rows() << " by " << second.columns() << " sparse matrix." << std::endl;
+            std::cout << "First one has a sparsity of " << first.sparsity() << "and it is " << (first.is_compressed() ? "in" : "not in") << " compressed form." << std::endl;
+            std::cout << "Second one has a sparsity of " << second.sparsity() << " and it is " << (second.is_compressed() ? "in" : "not in") << " compressed form." << std::endl;
+            std::cout << "Both matrices are " << (O == Row ? "row-first" : "column-first") << " ordered." << std::endl;
             std::cout << "Elapsed time: " << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() / 1E6 << " second(s)." << std::endl;
 
         }
